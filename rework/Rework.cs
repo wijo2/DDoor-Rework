c5975479f6b0e3a89d2323c3a6d787830dc028e0
valo keskevaari <valo.keskevaari@gmail.com>
2023-02-04 19:41:22 +0200

    made projectile slimes actually work

--- rework/Rework.cs
+++ rework/Rework.cs
@@ -974,6 +974,10 @@ namespace rework
         }
 
         //slime
+        //blood scale = timer for growing up
+        //blood offset x = 0.1 means don't spawn more slimes
+        //blood offset y = projectile slime's timer for when to start moving 
+
 
         //small grow up
         [HarmonyPatch(typeof(AI_Slime), "FixedUpdate")]
@@ -1003,16 +1007,35 @@ namespace rework
         //medium spawn small
         [HarmonyPatch(typeof(AI_Slime), "hopSpikeCheck")]
         [HarmonyPostfix]
-        public static void hopSpikeCheck_post(bool __result, AI_Slime __instance, ref int ___hopSpikeCounter)
+        public static void hopSpikeCheck_post(bool __result, AI_Slime __instance, ref int ___hopSpikeCounter, ref float ___angle)
         {
             if (!LoadThings("smallSlime")[0] || __instance.size != AI_Slime.SlimeSize.Medium || !__result || __instance.gameObject.GetComponent<DamageableCharacter>().bloodVolumeSpawnOffset.x == 0.1f)
             {
                 return;
             }
             var projectileSlime = Instantiate(smallSlime, __instance.gameObject.transform.position + Vector3.up * 5, Quaternion.identity);
+            projectileSlime.GetComponent<Rigidbody>().velocity = Vector3.up * 30f;
             projectileSlime.SetActive(true);
-            projectileSlime.gameObject.GetComponent<DamageableCharacter>().bloodVolumeSpawnOffset.x = 0.1f;
+            var dmg = projectileSlime.gameObject.GetComponent<DamageableCharacter>();
+            dmg.bloodVolumeSpawnOffset.x = 0.1f;
+            dmg.bloodVolumeSpawnOffset.y = 0.1f;
             ___hopSpikeCounter += 3;
+        }
+
+        //small slime stays still when launching
+        [HarmonyPatch(typeof(AI_Slime), "FixedUpdate")]
+        [HarmonyPrefix]
+        public static bool FixedUpdate_pre(AI_Slime __instance)
+        {
+            if (__instance.size != AI_Slime.SlimeSize.Small || __instance.gameObject.GetComponent<DamageableCharacter>().bloodVolumeSpawnOffset.y < 0f)
+            {
+                return true;
+            }
+            var dir = PlayerGlobal.instance.gameObject.transform.position - __instance.gameObject.transform.position;
+            dir.y = 0;
+            __instance.gameObject.GetComponent<Rigidbody>().velocity = dir * 1.5f + Vector3.up * (__instance.gameObject.GetComponent<Rigidbody>().velocity.y - 4f * Time.fixedDeltaTime);
+            __instance.gameObject.GetComponent<DamageableCharacter>().bloodVolumeSpawnOffset.y -= Time.fixedDeltaTime / 10f;
+            return false;
         }
     }
 }
